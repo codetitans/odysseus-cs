9e6bae91ff45833a6d6b4ecf9ac73e7d7518df8a
Paweł Hofman <pawel@codetitans.pl>
2025-04-04 15:42:49 +0200

    Updated tests

--- src/UnitTests/ClientPublishingTests.cs
+++ src/UnitTests/ClientPublishingTests.cs
@@ -12,4 +12,18 @@ public sealed class ClientPublishingTests
         client.Log("Test message");
         await Task.Delay(1000);
     }
+
+    [Fact]
+    public async Task publish_multiple_messages_at_once()
+    {
+        var client = new OdysseusClient("app_052c18d7d5154fd1b945b8173bf9eff2", "0M18_pCf5msaqu7Zd6b0C3lD", delay: 0);
+        var ts = DateTime.UtcNow;
+        var tag = "T1'";
+
+        client.SessionId = Guid.Parse("fd416bad-beef-418e-96c1-74a27dc4196e");
+        client.Log("Message-1", tag: tag, timestamp: ts);
+        client.Log("Message-2", tag: tag, timestamp: ts);
+        client.Log("Message-3", tag: tag, timestamp: ts);
+        await Task.Delay(1000);
+    }
 }
